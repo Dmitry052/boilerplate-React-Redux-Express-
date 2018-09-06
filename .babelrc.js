53125b27ad333b9584d352bc33be0caee23ca727
Dmitry <main.mailbox.vda@gmail.com>
2018-09-06 20:16:35 +0300

    Del stage 2

---
+++ .babelrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  presets: [],
+  ignore: ["node_modules"],
+  plugins: []
+};
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const ENVIRONMENT = process.env.NODE_ENV;
 const APP_HOST = process.env.APP_HOST || config[ENVIRONMENT].server.host;
 const APP_PORT =
   Number(process.env.APP_PORT) || config[ENVIRONMENT].server.port;
+const reStyle = /\.(css|less|scss|sss)$/;
 
 module.exports = {
   entry: { app: "./src/index.js" },
@@ -21,21 +22,68 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          options: {
+            babelrc: false,
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
         }
       },
+      {
+        test: reStyle,
+        rules: [
+          // Convert CSS into JS module
+          {
+            issuer: { not: [reStyle] },
+            use: "isomorphic-style-loader"
+          },
 
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader"]
-        })
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
+          // Process external/third-party styles
+          {
+            exclude: path.resolve(__dirname, "./src"),
+            loader: "css-loader",
+            options: {
+              sourceMap: false,
+              minimize: true,
+              discardComments: { removeAll: true }
+            }
+          },
+
+          // Process internal/project styles (from src folder)
+          {
+            include: path.resolve(__dirname, "./src"),
+            loader: "css-loader",
+            options: {
+              // CSS Loader https://github.com/webpack/css-loader
+              importLoaders: 1,
+              sourceMap: false,
+              // CSS Modules https://github.com/css-modules/css-modules
+              modules: true,
+              localIdentName: "[name]-[local]",
+              // CSS Nano http://cssnano.co/options/
+              minimize: true,
+              discardComments: { removeAll: true }
+            }
+          },
+
+          {
+            test: /\.scss$/,
+            loader: "sass-loader"
+          }
+        ]
       }
+      // {
+      //   test: /\.css$/,
+      //   use: ExtractTextPlugin.extract({
+      //     fallback: "style-loader",
+      //     use: ["css-loader"]
+      //   })
+      // },
+      // {
+      //   test: /\.scss$/,
+      //   use: ["style-loader", "css-loader", "sass-loader"]
+      // }
     ]
   },
   watch: ENVIRONMENT === "development" || false,
